1f8857c767477c8ba800eceabfbc1e848cd4cba7
Timur <you@example.com>
2025-05-24 14:41:22 +0300

    chore: add axios for HTTP requests

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Profile() {
+  useEffect(() => {
+    const getUserProfile = async() => {
+      try {
+        //const response = await
+      } catch (error) {
+        
+      }
+    }
+  }, [])
+  
   return <></>;
 }
